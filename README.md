20cde2da5a3d35b7144dbf5ea2fac2aa8651106a
GuolongWang <da60511@gmail.com>
2025-06-16 13:55:40 +0800

    benchmark gf16 ov_publicmap

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ printf "Sleep 2000\nLoadFile bench_ra8m1.elf\ngo\nSleep 3600000" | JLinkExe -if 
 | UOV-Is: gf16mat_prod 48_64    | 21453   |   52976 |            16093 |
 | UOV-Is: gf16mat_prod 32_X     | 3741    |    8491 |             2778 |
 | UOV-Is: gf16trimat_2trimat_madd 96_48_64_32| 103,977,285 |776,791,141|114,558,066|
+| GF16 ov_publicmap | 518,931 | 792,646 | |
 | UOV-Ip: gf256mat_prod 1936_68 | 1188551 | 3088048 |          1397865 |
 | UOV-Ip: gf256mat_prod 68_44   | 31962   |  127600 |            33342 |
 | UOV-Ip: gf256mat_prod 44_X    | 10511   |   31539 |            11261 |
--- benchmarks/main.c
+++ benchmarks/main.c
@@ -26,19 +26,26 @@
 #define BWIDTH       44
 #define SIZE_BATCH   44
 
-#define bench_cycles(CALL, OUT_VAR)                                                    \
+// uov_publicmap settings
+#define N 64
+#define M 64
+#define _MAX_N 256
+#define TMPVEC_LEN 32
+#define _PUB_N 64
+#define _PUB_M 64
+#define _V 32
+#define _PUB_M_BYTE (_PUB_M / 2)
+#define _GFSIZE 16
+
+#define bench_cycles(CALL, OUT_VAR)                                    \
     do {                                                               \
         __disable_irq();                                               \
-                                                                       \
         ARM_PMU_CYCCNT_Reset();                                        \
-                                                                       \
         ARM_PMU_CNTR_Enable(PMU_CNTENSET_CCNTR_ENABLE_Msk);            \
         CALL;                                                          \
         ARM_PMU_CNTR_Disable(PMU_CNTENCLR_CCNTR_ENABLE_Msk);           \
-                                                                       \
         printf(#CALL ": cycles = %" PRIu32 "\n", ARM_PMU_Get_CCNTR()); \
         OUT_VAR = ARM_PMU_Get_CCNTR();                                 \
-                                                                       \
         __enable_irq();                                                \
     }                                                                  \
     while (0)
@@ -80,7 +87,6 @@ static inline void _gf256v_add_u32(uint8_t *accu_b, const uint8_t *a, unsigned _
         accu_b[_num_byte] ^= a[_num_byte];
     }
 }
-
 
 static inline uint32_t gf256v_mul_u32(uint32_t a, uint8_t b) {
     uint32_t a_msb;
@@ -213,6 +219,100 @@ void gf256mat_prod_ref(uint8_t *c, const uint8_t *matA, unsigned n_A_vec_byte, u
     }
 }
 
+// uov_publicmap() start ===================================================
+static inline uint8_t gfv_get_ele(const uint8_t *a, unsigned i) {
+    uint8_t r = a[i >> 1];
+    return (i & 1) ? (r >> 4) : (r & 0xf);
+}
+
+// gf16 := gf2[x]/(x^4+x+1)
+static inline uint8_t gf16_mul(uint8_t a, uint8_t b) {
+    uint8_t r8 = (a & 1) * b;
+    r8 ^= (a & 2) * b;
+    r8 ^= (a & 4) * b;
+    r8 ^= (a & 8) * b;
+
+    // reduction
+    uint8_t r4 = r8 ^ (((r8 >> 4) & 5) * 3); // x^4 = x+1  , x^6 = x^3 + x^2
+    r4 ^= (((r8 >> 5) & 1) * 6);       // x^5 = x^2 + x
+    return (r4 & 0xf);
+}
+
+void gf16v_mul(uint8_t *a, uint8_t gf16_b, unsigned _num_byte) {
+    for(int i=0;i<_num_byte;i++){
+        a[i] = gf16_mul(a[i], gf16_b) | (gf16_mul(a[i]>>4, gf16_b) << 4);
+    }
+}
+
+void gf16v_madd(uint8_t *a, const uint8_t *mat, uint8_t gf16_b, unsigned _num_byte) {
+    for(int i=0;i<_num_byte;i++){
+        a[i] ^= gf16_mul(mat[i], gf16_b) | (gf16_mul(mat[i]>>4, gf16_b) << 4);
+    }
+}
+
+void gf16v_add(uint8_t *a, const uint8_t *b, unsigned _num_byte) {
+    for(int i=0;i<_num_byte;i++){
+        a[i] ^= b[i];
+    }
+}
+\
+void ov_publicmap( unsigned char *y, const unsigned char *trimat, const unsigned char *x ) {
+    unsigned char _xixj[_MAX_N] = {0};
+    unsigned v = _V;
+    unsigned o = _PUB_N - _V;
+    unsigned char _x[_MAX_N];
+
+    for (unsigned i = 0; i < _PUB_N; i++) {
+        _x[i] = gfv_get_ele( x, i );
+    }
+    unsigned int vec_len = _PUB_M_BYTE;
+
+    // P1
+    for (unsigned i = 0; i < _V; i++) {
+        unsigned i_start = i - (i & 3);
+        for (unsigned j = i; j < _V; j++) {
+            _xixj[j] = _x[j];
+        }
+        
+        gf16v_mul( _xixj + i_start, _x[i], v - i_start );
+        
+        for (unsigned j = i; j < v; j++) {
+            gf16v_madd(y, trimat, _xixj[j], vec_len);
+            trimat += vec_len;
+        }
+    }
+
+    // P2
+    for (unsigned i = 0; i < v; i++) {
+        for (unsigned j = 0; j < o; j++) {
+            _xixj[j] = _x[v + j];
+        }
+
+        gf16v_mul( _xixj, _x[i], o );
+
+        for (unsigned j = 0; j < o; j++) {
+            gf16v_madd(y, trimat, _xixj[j], vec_len);
+            trimat += vec_len;    
+        }
+    }
+
+    // P3
+    for (unsigned i = 0; i < o; i++) {
+        unsigned i_start = i - (i & 3);
+        for (unsigned j = i; j < o; j++) {
+            _xixj[j] = _x[v + j];
+        }
+
+        gf16v_mul( _xixj + i_start, _x[v + i], o - i_start );
+        
+        for (unsigned j = i; j < o; j++) {
+            gf16v_madd(y, trimat, _xixj[j], vec_len);
+            trimat += vec_len;
+        }
+    }
+}
+// uov_publicmap() end ===================================================
+
 void gf256mat_prod_1936_68(uint8_t *c, const uint8_t *matA, const uint8_t *b);
 void gf256mat_prod_68_44(uint8_t *c, const uint8_t *matA, const uint8_t *b);
 void gf256mat_prod_44_X(uint8_t *c, const uint8_t *matA, const uint8_t *b, size_t n_A_width);
@@ -220,6 +320,10 @@ void gf256mat_prod_44_X(uint8_t *c, const uint8_t *matA, const uint8_t *b, size_
 void gf256mat_prod_m4f_1936_68_normal_normal(uint32_t *c, uint32_t *a, uint8_t *b);
 void gf256mat_prod_m4f_68_44_normal_normal(uint32_t *c, uint32_t *a, uint8_t *b);
 void gf256mat_prod_m4f_44_X_normal_normal(uint32_t *c, uint32_t *a, uint8_t *b, size_t n_A_width);
+void gf256trimat_2trimat_madd_m4f_68_68_44_44(uint32_t *c, uint32_t *a, uint8_t *b);
+
+void batch_2trimat_madd_gf256_mve( unsigned char *bC, const unsigned char *btriA,
+    const unsigned char *B, unsigned Bheight, unsigned size_Bcolvec, unsigned Bwidth, unsigned size_batch );
 
 void batch_2trimat_madd_gf256( unsigned char *bC, const unsigned char *btriA,
     const unsigned char *B, unsigned Bheight, unsigned size_Bcolvec, unsigned Bwidth, unsigned size_batch ) {
@@ -249,10 +353,6 @@ void batch_2trimat_madd_gf256( unsigned char *bC, const unsigned char *btriA,
         }
     }
 }
-
-void batch_2trimat_madd_gf256_mve( unsigned char *bC, const unsigned char *btriA,
-    const unsigned char *B, unsigned Bheight, unsigned size_Bcolvec, unsigned Bwidth, unsigned size_batch );
-void gf256trimat_2trimat_madd_m4f_68_68_44_44(uint32_t *c, uint32_t *a, uint8_t *b);
 
 static void print_u64(uint64_t v){
     /* 1 000 000 000 < 2^32，可安全存進 uint32_t */
@@ -320,12 +420,61 @@ void benchmark_gf256trimat_2trimat_madd_68_68_44_44(){
     print_u64(sum_m4 / TEST_RUN);
 }
 
+void ov_publicmap_mve( unsigned char *y, const unsigned char *trimat, const unsigned char *x );
+
+void benchmark_ov_publicmap(){
+    uint32_t sum_ref = 0, sum_mve = 0;
+    uint32_t cycles;
+
+    unsigned char P[M * N * (N + 1) / 4];
+    unsigned char sig[N / 2];
+    unsigned char acc[M / 2], acc_mve[M / 2];
+
+    int fail = 0;
+    for (int i = 0; i < TEST_RUN; i++) {
+        memset(acc, 0, sizeof(acc));
+        memset(acc_mve, 0, sizeof(acc_mve));
+        randombytes(P, sizeof(P));
+        randombytes(sig, sizeof sig);
+
+        bench_cycles(ov_publicmap(acc, P, sig), cycles);
+        sum_ref += cycles;
+        bench_cycles(ov_publicmap_mve(acc_mve, P, sig), cycles);
+        sum_mve += cycles;
+
+        if(memcmp(acc_mve, acc, sizeof(acc))){
+            printf("acc_ref = [");
+            for (unsigned k = 0; k < sizeof(acc); k++) {
+                printf("%02x ", acc[k]);
+            }
+            printf("]\n");
+
+            printf("acc_mve = [");
+            for (unsigned k = 0; k < sizeof(acc_mve); k++) {
+                printf("%02x ", acc_mve[k]);
+            }
+            printf("]\n"); 
+            fail = 1;
+            break;
+        }   
+    } 
+
+    printf((fail) ? "TEST FAIL.!\n" : "TEST PASS.\n");
+    printf("Average ref cycles = %lu\n", sum_ref / TEST_RUN);
+    printf("Average MVE cycles = %lu\n", sum_mve / TEST_RUN);
+}
+
 ITCM_FN int main (void)
 {
     Utils_Init();
     PMU_Init();
+    
+    // These three benchmarks can't load on the board
+    // benchmark_gf256mat_prod_1936_68();
+    // benchmark_gf256mat_prod_68_44();
+    // benchmark_gf256mat_prod_44_X();
     benchmark_gf256trimat_2trimat_madd_68_68_44_44();
-    printf("nice\n");
+    benchmark_ov_publicmap();
     return( 0 );
 }
 #endif
